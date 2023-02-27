c0da3da723a909ca799c6f09fe51a02c7eefcd62
AyushKumawat11 <ayushkumawat1122@gmail.com>
2023-02-27 17:38:02 +0530

    installed npm and parcel

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const title = <h1 id="h1">Hello World....</h1>
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(title);
